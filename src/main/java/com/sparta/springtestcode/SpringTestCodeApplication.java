2bb0f4ff629a77033269a8048fb1bbce0811338f
hoon <dotofi@naver.com>
2024-07-05 14:48:31 +0900

    init commit

---
+++ src/main/java/com/sparta/springtestcode/SpringTestCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springtestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestCodeApplication.class, args);
+    }
+
+}
